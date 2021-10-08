f22253cf68b445a0d0eb558e505944f3d239557b
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-10-08 18:54:55 +0300

    modified details.html and updatated its ts file

--- src/app/component/quote-details/quote-details.component.ts
+++ src/app/component/quote-details/quote-details.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quotes } from 'src/app/quotes';
+Quotes
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+
+  @Input() quote:Quotes;
+
+  @Output() isRead = new EventEmitter<boolean>();
+
+  deleteQuote(read:boolean){
+    
+    this.isRead.emit(read);
+  }
+  upvote(){
+    this.quote.upvote+=1;
+  }
+  downvote(){
+    this.quote.downvote+=1;
+  }
+
 
   constructor() { }
 

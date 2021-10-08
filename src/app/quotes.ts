08d5285ddc938c47e5f0a06ec2844e31d3e65522
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-10-08 18:33:38 +0300

    modified quoute.html updatated its ts file and quote class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,9 @@
 export class Quotes {
-}
+
+    constructor ( public quote:string, public author: string, public publisher:string, public downvote:number, public upvote:number, public published:Date
+        ){
+    
+      }
+    }
+
+
